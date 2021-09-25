465ed8ea4c76984d154fbbf9a41c73e52831ea54
Zack Borges <zackartborges@gmail.com>
2021-09-25 15:33:21 -0400

    adds css styling for meme and text

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header>
        <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
-                alt="Problem?"
+          src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+          alt="Problem?"
             />
       <h1>Meme Generator</h1>
     </header>
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -45,8 +45,12 @@ class MemeGenerator extends Component {
                 onChange={this.handleChange}
               />
             </form>
-            <h1>MEME GENERATOR SECTION</h1>
-                </div>
+            <div className = "meme">
+              <img src={this.state.randomImg} alt="" />
+              <h2 className="top">{this.state.topText}</h2>
+              <h2 className="bottom">{this.state.bottomText}</h2>
+            </div>
+          </div>
         )
     }
 }
