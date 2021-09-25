518cd8c0f05f6b71ee8d774b3d77645ab77ac7cf
Zack Borges <zackartborges@gmail.com>
2021-09-25 14:28:39 -0400

    creates class based meme generator component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js'
+import MemeGenerator from './MemeGenerator.js'
+
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <MemeGenerator />
     </div>
   );
 }
---
+++ src/MemeGenerator.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
+    }
+    
+    render() {
+        return (
+            <h1>MEME GENERATOR SECTION</h1>
+        )
+    }
+}
+
+export default MemeGenerator
